31e435c07c7a1353204c62c8dda6ad73a3bb9c1f
Ayman Tauhid <aymantauhid2@gmail.com>
2025-11-18 16:45:37 -0500

    Initial Commit

---
+++ 7Shifts_FoodMart/_Shifts_FoodMartApp.swift
@@ -0,0 +1,17 @@
+//
+//  _Shifts_FoodMartApp.swift
+//  7Shifts_FoodMart
+//
+//  Created by Ayman Tauhid on 2025-11-18.
+//
+
+import SwiftUI
+
+@main
+struct _Shifts_FoodMartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ 7Shifts_FoodMartTests/_Shifts_FoodMartTests.swift
@@ -0,0 +1,17 @@
+//
+//  _Shifts_FoodMartTests.swift
+//  7Shifts_FoodMartTests
+//
+//  Created by Ayman Tauhid on 2025-11-18.
+//
+
+import Testing
+@testable import _Shifts_FoodMart
+
+struct _Shifts_FoodMartTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
